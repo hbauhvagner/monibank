0c61d2e7431bae4a6efc42704adf6fa639b0fad9
hbauhvagner <hbauhvagner@gmail.com>
2023-07-13 23:22:01 -0300

    CPF: primeiro digito

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -2,6 +2,7 @@ export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
 
     validaNumerosRepetidos(cpf);
+    validaPrimeiroDigito(cpf);
 
     console.log(validaNumerosRepetidos(cpf));
 }
@@ -21,4 +22,22 @@ function validaNumerosRepetidos(cpf) {
     ];
 
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
 }
